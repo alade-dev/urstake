4b4f2eb3e137b28d8dd1da5d63b0aa6507c2f974
Teslim Ibisomi <ibisomimayowa@gmail.com>
2025-09-16 11:42:59 +0100

    Fix template string syntax in footer copyright message

--- client/src/pages/Index.tsx
+++ client/src/pages/Index.tsx
@@ -118,7 +118,7 @@ const Index = () => {
 
           <div className="border-t border-gray-700 mt-12 pt-8 flex flex-col md:flex-row justify-between items-center">
             <p className="text-gray-400 text-sm">
-              © ${new Date().getFullYear()} UrStake. All rights reserved.
+              © {new Date().getFullYear()} UrStake. All rights reserved.
             </p>
             <div className="flex space-x-6 mt-4 md:mt-0">
               <a
